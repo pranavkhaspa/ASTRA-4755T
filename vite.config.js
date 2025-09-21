8a8383444be9f33429b55c92c7b41652e8bff044
pranavkhaspa <158171302+pranavkhaspa@users.noreply.github.com>
2025-09-21 10:02:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['jspdf'], // forces Vite to pre-bundle it
-  },
   build: {
     rollupOptions: {
-      external: ['jspdf'], // treat it as external (won’t bundle)
+      external: ['jspdf'], // treat jspdf as external
     },
   },
 });
